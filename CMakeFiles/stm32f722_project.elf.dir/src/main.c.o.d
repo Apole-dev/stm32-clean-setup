7aca29e93c624111d86397359514c8699ca2a823
Apole-dev <Apole.dev@gmail.com>
2025-01-13 21:58:08 +0300

    update

--- CMakeFiles/stm32f722_project.elf.dir/src/main.c.o.d
+++ CMakeFiles/stm32f722_project.elf.dir/src/main.c.o.d
@@ -2,13 +2,13 @@ CMakeFiles/stm32f722_project.elf.dir/src/main.c.o: \
  /home/eren/Projects/stm_bare_metal/src/main.c \
  /home/eren/Projects/stm_bare_metal/include/stm32f722xx.h \
  /home/eren/Projects/stm_bare_metal/include/core_cm7.h \
- /usr/lib/gcc/arm-none-eabi/14.1.0/include/stdint.h \
- /usr/arm-none-eabi/include/stdint.h \
- /usr/arm-none-eabi/include/machine/_default_types.h \
- /usr/arm-none-eabi/include/sys/features.h \
- /usr/arm-none-eabi/include/_newlib_version.h \
- /usr/arm-none-eabi/include/sys/_intsup.h \
- /usr/arm-none-eabi/include/sys/_stdint.h \
+ /opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/lib/gcc/arm-none-eabi/14.2.1/include/stdint.h \
+ /opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/stdint.h \
+ /opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ /opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ /opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ /opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
+ /opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/_stdint.h \
  /home/eren/Projects/stm_bare_metal/include/cmsis_version.h \
  /home/eren/Projects/stm_bare_metal/include/cmsis_compiler.h \
  /home/eren/Projects/stm_bare_metal/include/cmsis_gcc.h \
--- src/main.c
+++ src/main.c
@@ -7,24 +7,35 @@ void SystemInit(void);
 void delay(volatile uint32_t count) {
   while (count--)
     ;
+
 }
 
+void LED_INIT(void);
+
 int main(void) {
-  //
+ 
   void SystemInit(void); 
-  // LD1 LED'i (PB0)
-  // RCC - Port B clock enable
-  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
-
-  GPIOB->MODER &= ~(3U << (0 * 2));
-  GPIOB->MODER |= (1U << (0 * 2));
-
+  
+  LED_INIT();
 
   while (1) {
     // LED Toggle
     GPIOB->ODR ^= (1U << 0);
     delay(1000000);
   }
+}
+
+void LED_INIT(void){
+
+  //Clock Enable
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+
+  /*
+   * MODE SET */
+  
+  GPIOB->MODER &= ~(3U << (0*2));
+  GPIOB->MODER |= (1U << (0*2));
+
 }
 
 void NMI_Handler(void) {
